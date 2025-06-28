786ff3ab43ed9d4d153027e1292b1daf5e89e01b
manoj vishwakarma <106471320+Manoj2886@users.noreply.github.com>
2025-06-28 16:44:36 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,120 @@
+
+    <!--*Header*-->
+    <?php include 'includes/header.php';?>	
+    <!--* End Header*-->
+
+    <!-- Banner -->
+    <div id="blog_banner">
+        <div class="page-title">
+            <div class="container">
+                <h2>About Us</h2>
+            </div>
+        </div>
+        <div class="black-overlay"></div>
+    </div>
+    <!-- End banner -->
+
+    <!-- breadcrumb -->
+    <div class="breadcrumb-main">    
+        <div class="container">        
+            <ul class="breadcrumb">
+                <li><a href="index.html">Home</a></li>
+                <li class="active">About Us</li>
+            </ul>
+        </div>
+    </div>
+    <!-- End breadcrumb -->
+
+    <!--*About*-->
+    <section id="mt_about" class="mm">
+        <div class="container">
+            <div class="about_services">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="about-items">
+                            <div class="inner-heading hide">
+                                <h2 class="mar-0">About Us</h2>
+								
+                            </div>
+                            <h3>About Mstechvision Computer Institute</h3>
+                            <p>Estb August 2020 Mstechvision Computer Institute has been ranked among the top institute. The standards of teaching , research and training in institute are equaled by very few institution today, the industries need well trained people to keep with pace at which the changes are taking place in the areas of communication , management and information technology and instilling in the students the value of righteousness, integrity and truth.
+
+We at Mstechvision Computer Institute committed to provide quality training of international standards through experts in the latest Technology to rendor the best and timely With Dedicated staff possessing strong human values.
+
+We shall provide better coaching to inculcate leadership Skills in the young generation so that they carry our Nature to new horizons, which will help them to develop into well rounded personalities so that they are useful Citizens of the country tomorrow.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--*EndAbout*-->
+
+    <!-- About Courses -->
+    <!-- End About Courses -->
+
+    <!--* Mission Area*-->
+    <section id="mission_main" class="mission-area section-padding ab-ineer" style="background: #f1f0f0;"> 
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-6 col-xs-12">
+                    <div class="mission-content text-center">
+                        <div class="inner-heading ab-hed">
+                            <h2>Our Mission </h2>
+                        </div>
+                        <p>We at the Mstechvision Computer Institute committed to provide quality mission to create and sustain a coaching in which students acquire knowledge and learn to apply it professionally with due consideration for explore the possibilities of whole world by remaining confidents' firm about their decision's keep focusing on their goal to satisfy the needs of country and society.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6 col-xs-12">
+					<div class="mission-content text-center">
+                        <div class="inner-heading ab-hed">
+                            <h2>Our Vision</h2>
+                        </div>
+                        <p>We at Mstechvision Computer Institute committed to provide quality vision to be an academic institution in dynamic equilibrium with its social ecological and economic environment striving continuously for excellence to imbibe rich ethical values in the young generation so that they can work for the prosperity of our nation by achieving success.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+	
+
+    <!--*Footer*-->
+   <?php include 'includes/footer.php';?>	
+    <!--* End Footer*-->
+    <!-- back to top -->
+    <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="" data-placement="left">
+        <span class="fa fa-arrow-up"></span>
+    </a>
+    <!--*Scripts*-->
+
+    <!-- Latest jquery --><script src="js/jquery-3.2.1.min.js"></script>
+
+    <!-- latest Bootstrap --><script src="js/bootstrap.min.js"></script>
+
+    <!-- Parallax jquery --><script src="js/jquery.parallax-1.1.3.js"></script>
+    
+    <!-- Fancybox jquery --><script src="js/jquery.fancybox.pack.js"></script>
+
+    <!-- wow animated jquery --><script src="js/wow.min.js"></script>
+
+    <!-- Navigation jquery --><script src="js/jquery.nav.js"></script>
+
+    <!-- Slick jquery --><script src="js/slick.js"></script>
+
+    <!-- Slicknav jquery --><script src="js/slicknav.js"></script>
+
+    <!-- custom nav jquery --><script src="js/custom-nav.js"></script>
+
+    <!-- Owl Carousel Jquery --><script src="js/owl.carousel.min.js"></script>
+
+    <!-- Easing Jquery --><script src="js/jquery.easing.min.js"></script>
+
+    <!-- Appear Jquery --><script src="js/jquery.appear.js"></script>
+
+    <!-- isotope Jquery --><script src="js/isotope.min.js"></script>
+
+    <!-- CountTo Jquery --><script src="js/jquery.countTo.js"></script>
+
+    <!-- main default Jquery --><script src="js/main.js"></script>
+</body>
+</html>
